5e7b64bfdf76838b68a74e3cf18237b2e3471761
Les Aker <me@lesaker.org>
2015-10-29 00:18:21 -0400

    fix group for mac

--- manifests/base.pp
+++ manifests/base.pp
@@ -7,7 +7,7 @@ class sysctl::base (
   $values             = undef,
   $sysctl_dir_path    = '/etc/sysctl.d',
   $sysctl_dir_owner   = 'root',
-  $sysctl_dir_group   = 'root',
+  $sysctl_dir_group   = 'wheel',
   $sysctl_dir_mode    = '0755'
 ) {
 
