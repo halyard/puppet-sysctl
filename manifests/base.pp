8d570788e4be1f4e96d9999da31ff8f2ec3a6a46
Les Aker <me@lesaker.org>
2015-10-29 00:12:20 -0400

    clean up cops

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,23 +5,15 @@
 class sysctl::base (
   $purge              = false,
   $values             = undef,
-  $hiera_merge_values = false,
-  $symlink99          = $::sysctl::params::symlink99,
-  $sysctl_dir         = $::sysctl::params::sysctl_dir,
-  $sysctl_dir_path    = $::sysctl::params::sysctl_dir_path,
-  $sysctl_dir_owner   = $::sysctl::params::sysctl_dir_owner,
-  $sysctl_dir_group   = $::sysctl::params::sysctl_dir_group,
-  $sysctl_dir_mode    = $::sysctl::params::sysctl_dir_mode,
-) inherits ::sysctl::params {
+  $sysctl_dir         = false,
+  $sysctl_dir_path    = '/etc/sysctl.d',
+  $sysctl_dir_owner   = 'root',
+  $sysctl_dir_group   = 'root',
+  $sysctl_dir_mode    = '0755'
+) {
 
-  # Hiera support
-  if $hiera_merge_values == true {
-    $values_real = hiera_hash('sysctl::base::values')
-  } else {
-    $values_real = $values
-  }
-  if $values_real != undef {
-    create_resources(sysctl,$values_real)
+  if $values != undef {
+    create_resources(sysctl, $values)
   }
 
   if $sysctl_dir {
@@ -39,13 +31,5 @@ class sysctl::base (
       purge   => $purge,
       recurse => $recurse,
     }
-    if $symlink99 and $sysctl_dir_path =~ /^\/etc\/[^\/]+$/ {
-      file { "${sysctl_dir_path}/99-sysctl.conf":
-        ensure => link,
-        target => '../sysctl.conf',
-      }
-    }
   }
-
 }
-
